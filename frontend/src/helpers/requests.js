4c62bbcade7ff11935587a8ede09b1690dc802d7
Maxycle <maxycle@hotmail.com>
2024-05-20 11:50:36 +0200

    fix: use sessionStore

--- frontend/src/helpers/requests.js
+++ frontend/src/helpers/requests.js
@@ -1,12 +1,15 @@
 import { useTrackStore } from '@/stores/modules/tracks.js';
+import { useSessionStore } from '@/stores/modules/sessionStore';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000'
 // Function to fetch tracks from the API
+
 export async function fetchTracks(url) {
-  const store = useTrackStore();
+  const storeTrack = useTrackStore();
+	const storeSession = useSessionStore();
   try {
-    const fetchedTracks = await store.searchTracks(url);
+    const fetchedTracks = await storeTrack.searchTracks(url, storeSession.getAuthToken);
     console.log('Fetched tracks:', fetchedTracks);
     return fetchedTracks;
   } catch (error) {
@@ -17,9 +20,10 @@ export async function fetchTracks(url) {
 
 // Function to fetch user's tracks from the API
 export async function fetchMyTracks() {
-  const store = useTrackStore();
+  const storeTrack = useTrackStore();
+	const storeSession = useSessionStore();
   try {
-    const fetchedMyTracks = await store.searchTracks('/my_tracks');
+    const fetchedMyTracks = await storeTrack.searchTracks('/my_tracks', storeSession.getAuthToken);
     console.log('Fetched my tracks in requests:', fetchedMyTracks);
     return fetchedMyTracks;
   } catch (error) {
