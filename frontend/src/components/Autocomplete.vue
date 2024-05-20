7ee3d09e91aee4d86af40b57ea5030698a0285e3
Maxycle <maxycle@hotmail.com>
2024-05-20 12:21:13 +0200

    fix: reset selectedItem

--- frontend/src/components/Autocomplete.vue
+++ frontend/src/components/Autocomplete.vue
@@ -12,7 +12,7 @@
 					<div v-else> {{ item.name }}</div>
 				</li>
 			</ul>
-			<button v-else-if="selectedItem !== '' && canAddNewOption" class="p-2 mt-2 w-full bg-green-300 border border-dotted border-black rounded" @click="addNewOption">Add "{{
+			<button v-else-if="selectedItem !== '' && canAddNewOption && !filteredItems.length" class="p-2 mt-2 w-full bg-green-300 border border-dotted border-black rounded" @click="addNewOption">Add "{{
 				selectedItem }}"</button>
 		</div>
 	</div>
@@ -86,6 +86,7 @@ export default {
 			} else {
 				this.selectedItem = item.name
 				this.$emit('item-selected', { header: this.heading, name: this.selectedItem, id: item.id })
+				this.selectedItem = ''
 			}
 			this.filteredItems = []
 		},
