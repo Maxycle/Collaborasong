84e6f32a5937b86e210e8a7379043bc499be8d72
Maxycle <maxycle@hotmail.com>
2024-06-03 10:32:37 +0200

    fix: add {} to import

--- frontend/src/pages/Chat.vue
+++ frontend/src/pages/Chat.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="chat">
-    <div class="left">
+	<div id="chat">
+		<!-- eslint-disable-next-line max-len -->
+		<div class="left">
       <nav>
         <!-- eslint-disable-next-line max-len -->
         <button v-for="chatroom in chatroomStore.getChatrooms" :key="chatroom.id" @click="moveToChatroom(chatroom.id)">
@@ -10,194 +11,191 @@
       <!-- <router-link :to="{ name: 'Logout' }">Log out</router-link> -->
     </div>
 
-    <div class="content">
-      <div ref="messagesElement" class="messages">
-        <!-- eslint-disable-next-line max-len -->
-        <Message
-          v-for="(message, index) in chatroomStore.getMessages"
-          :key="message.id"
-          :message="message"
-          :last-message="(index === 0) ? null : chatroomStore.getMessages[index - 1]"
-        />
-      </div>
+		<div class="content">
+			<div ref="messagesElement" class="messages">
+				<!-- eslint-disable-next-line max-len -->
+				<Message v-for="(message, index) in chatroomStore.getMessages" :key="message.id" :message="message"
+					:last-message="(index === 0) ? null : chatroomStore.getMessages[index - 1]" />
+			</div>
 
-      <form @submit.prevent="messagesCreate">
-        <textarea v-model="newMessage" rows="3" @keyup.enter.exact.prevent="messagesCreate" />
-        <button><FontAwesomeIcon icon="fa-solid fa-paper-plane" class="send" /></button>
-      </form>
-    </div>
-  </div>
+			<form @submit.prevent="messagesCreate">
+				<textarea v-model="newMessage" rows="3" @keyup.enter.exact.prevent="messagesCreate" />
+				<button class="bg-red-400">
+					<FontAwesomeIcon icon="fa-solid fa-paper-plane" class="send" />
+				</button>
+			</form>
+		</div>
+	</div>
 </template>
 
 <script setup>
-  import { ref, nextTick, onBeforeUnmount, onMounted } from "vue"
-  import { createConsumer } from "@rails/actioncable"
+import { ref, nextTick, onBeforeUnmount, onMounted } from "vue"
+import { createConsumer } from "@rails/actioncable"
 
-  import useChatroomStore from "@/stores/modules/chatroomStore"
-  import Message from "@/components/Message.vue"
+import { useChatroomStore } from "@/stores/modules/chatroomStore"
+import Message from "@/components/Message.vue"
 
-  const chatroomStore = useChatroomStore()
-  const actionCableConsumer = createConsumer("ws://localhost:3000/cable")
-  let channel
+const chatroomStore = useChatroomStore()
+const actionCableConsumer = createConsumer("ws://localhost:3000/cable")
+let channel
 
-  const messagesElement = ref(null)
-  const newMessage = ref("")
+const messagesElement = ref(null)
+const newMessage = ref("")
 
-  const scrollDown = () => {
-    nextTick(() => {
-      messagesElement.value.lastElementChild.scrollIntoView()
-    })
-  }
+const scrollDown = () => {
+	nextTick(() => {
+		messagesElement.value.lastElementChild.scrollIntoView()
+	})
+}
 
-  const addMessage = data => {
-    chatroomStore.addMessage(data)
-    scrollDown()
-  }
+const addMessage = data => {
+	chatroomStore.addMessage(data)
+	scrollDown()
+}
 
-  const subscribeToChannel = () => {
-    channel = actionCableConsumer.subscriptions.create(
-      { channel: "ChatroomChannel", id: chatroomStore.getChatroomId },
-      { received: data => addMessage(data) }
-    )
-  }
+const subscribeToChannel = () => {
+	channel = actionCableConsumer.subscriptions.create(
+		{ channel: "ChatroomChannel", id: chatroomStore.getChatroomId },
+		{ received: data => addMessage(data) }
+	)
+}
 
-  const messagesCreate = async () => {
-    await chatroomStore.messagesCreate(newMessage.value)
+const messagesCreate = async () => {
+	await chatroomStore.messagesCreate(newMessage.value)
 
-    newMessage.value = ""
-  }
+	newMessage.value = ""
+}
 
-  const moveToChatroom = async id => {
+const moveToChatroom = async id => {
     await chatroomStore.updateChatroomId(id)
     scrollDown()
     channel.unsubscribe()
     subscribeToChannel()
   }
 
-  onMounted(async () => {
-    await chatroomStore.chatroomsIndex()
-    await chatroomStore.messagesIndex()
+onMounted(async () => {
+	await chatroomStore.chatroomsIndex()
+	await chatroomStore.messagesIndex()
+	scrollDown()
+	subscribeToChannel()
+})
 
-    scrollDown()
-
-    subscribeToChannel()
-  })
-
-  onBeforeUnmount(() => channel.unsubscribe())
+onBeforeUnmount(() => channel.unsubscribe())
 </script>
 
 <style lang="scss" scoped>
-  #chat {
-    display: flex;
-    height: 100vh;
-    overflow: hidden;
-    width: 100vw;
+#chat {
+	display: flex;
+	height: auto;
+	overflow: hidden;
+	width: 100vw;
 
-    .left {
-      background-color: #14141a;
-      border-right: 1px solid #d3d3d359;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      width: 180px;
+	.left {
+		background-color: #14141a;
+		border-right: 1px solid #d3d3d359;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		width: 180px;
 
-      a {
-        border-radius: 5px;
-        border-top: 1px solid #d3d3d359;
-        color: yellowgreen;
-        font-size: 0.8rem;
-        padding: 10px 20px;
-        transition: color 0.3s ease;
-        width: 100%;
+		a {
+			border-radius: 5px;
+			border-top: 1px solid #d3d3d359;
+			color: yellowgreen;
+			font-size: 0.8rem;
+			padding: 10px 20px;
+			transition: color 0.3s ease;
+			width: 100%;
 
-        &:hover {
-          color: blueviolet;
-        }
-      }
+			&:hover {
+				color: blueviolet;
+			}
+		}
 
-      nav {
-        display: flex;
-        flex-direction: column;
-        padding: 20px 10px;
+		nav {
+			display: flex;
+			flex-direction: column;
+			padding: 20px 10px;
 
-        button {
-          border-radius: 5px;
-          color: red;
-          display: flex;
-          gap: 10px;
-          padding: 0.25rem 0.5rem;
-          transition: background-color 0.3s ease;
-          width: 100%;
+			button {
+				border-radius: 5px;
+				color: red;
+				display: flex;
+				gap: 10px;
+				padding: 0.25rem 0.5rem;
+				transition: background-color 0.3s ease;
+				width: 100%;
 
-          &:hover {
-            background-color: rgba(250, 250, 250, 0.1);
-          }
-        }
-      }
-    }
+				&:hover {
+					background-color: rgba(250, 250, 250, 0.1);
+				}
+			}
+		}
+	}
 
-    .content {
-      display: flex;
-      flex-direction: column;
-      flex-grow: 1;
-      padding: 20px 0;
+	.content {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		padding: 20px 0;
+		background-color: #12eb2f;
+		
+		form {
+			background-color: #191d20;
+			border: 1px solid yellow;
+			border-radius: 10px;
+			display: flex;
+			flex-direction: column;
+			flex-shrink: 0;
+			gap: 5px;
+			margin: 0 20px;
+			overflow: hidden;
+			padding: 10px;
+			width: calc(100% - 40px);
 
-      form {
-        background-color: #191d20;
-        border: 1px solid yellow;
-        border-radius: 10px;
-        display: flex;
-        flex-direction: column;
-        flex-shrink: 0;
-        gap: 5px;
-        margin: 0 20px;
-        overflow: hidden;
-        padding: 10px;
-        width: calc(100% - 40px);
+			button {
+				align-self: flex-end;
+				color: yellowgreen;
+				font-size: 0.8rem;
+				width: max-content;
+			}
 
-        button {
-          align-self: flex-end;
-          color: yellowgreen;
-          font-size: 0.8rem;
-          width: max-content;
-        }
+			textarea {
+				background-color: transparent;
+				border: none;
+				color: yellow;
+				font-family: inherit;
+				resize: none;
 
-        textarea {
-          background-color: transparent;
-          border: none;
-          color: inherit;
-          font-family: inherit;
-          resize: none;
+				&:focus {
+					outline: none;
+				}
+			}
+		}
 
-          &:focus {
-            outline: none;
-          }
-        }
-      }
+		.messages {
+			display: flex;
+			flex-direction: column;
+			flex-grow: 1;
+			margin: 0 5px 15px;
+			padding: 0 15px;
+			max-height: 100%;
+			overflow: hidden;
+			overflow-y: scroll;
 
-      .messages {
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
-        margin: 0 5px 15px;
-        padding: 0 15px;
-        max-height: 100%;
-        overflow: hidden;
-        overflow-y: scroll;
+			&:hover::-webkit-scrollbar-thumb {
+				background-color: #d3d3d359;
+				border-radius: 5px;
+			}
 
-        &:hover::-webkit-scrollbar-thumb {
-          background-color: #d3d3d359;
-          border-radius: 5px;
-        }
+			&::-webkit-scrollbar {
+				width: 5px;
+			}
 
-        &::-webkit-scrollbar {
-          width: 5px;
-        }
-
-        &::-webkit-scrollbar-thumb {
-          background-color: transparent;
-        }
-      }
-    }
-  }
+			&::-webkit-scrollbar-thumb {
+				background-color: transparent;
+			}
+		}
+	}
+}
 </style>
